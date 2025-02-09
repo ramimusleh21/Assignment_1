2735b58fd4c7413b65bedac9b55e602ac8a9e4cc
Rami Musleh <Rmusleh8054@conestogac.on.ca>
2025-02-09 17:45:20 -0500

    Added break to each case to ensure that both functions don't run when the user selects the first option.

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 void printWelcomeMenu();
 void printOptions();
 void add();
+void subtract();
 
 
 void main() {
@@ -20,6 +21,10 @@ void main() {
 	{
 	case 1:
 		add();
+		break;
+	case 2:
+		subtract();
+		break;
 	}
 
 }
@@ -46,4 +51,12 @@ void add() {
 	printf("%lf + %lf = %lf\n", num1, num2, result);
 }
 
-lsadhjflkjasdlfa
+void subtract() {
+	double num1, num2, result;
+	printf("Enter the first value:");
+	scanf_s("%lf", &num1);
+	printf("Enter the second value:");
+	scanf_s("%lf", &num2);
+	result = num1 - num2;
+	printf("%lf - %lf = %lf\n", num1, num2, result);
+}
